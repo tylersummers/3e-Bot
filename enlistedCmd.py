fdab156c8237e7e00f5e4313a10301ef709bf34a
Danx <danx@danx.live>
2026-07-26 23:22:03 +1000

    refactor: Remove unused medalsChannel variable from !medals command

--- enlistedCmd.py
+++ enlistedCmd.py
@@ -219,7 +219,6 @@ class enlistedCog(commands.Cog):
     @commands.command(aliases=["Medals"])
     async def medals(self, ctx):
         medalsChannel = self.bot.get_channel(772921078095937567)
-        await ctx.send(file=discord.File('/home/container/files/medals.png'))
   
         # Enlisted Medals Title ----------------------------------------------------
         enlistmedalsEmbed=discord.Embed(title="Enlisted Medals", description="", color=0xff0000)
